6da82a08c9cd30987a479c8768a8029bbee9fac2
Dusknior <danielbernalherrera@gmail.com>
2021-11-04 12:13:11 -0500

    first commit

---
+++ src/components/ControlPresupuesto.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { revisarPresupuesto } from "../helpers";
+
+const ControlPresupuesto = ({ restante, presupuesto }) => {
+  return (
+    <Fragment>
+      <div className="alert alert-primary">Presupueto: $ {presupuesto}</div>
+      <div className={revisarPresupuesto(presupuesto, restante)}>
+        Restante: $ {restante}
+      </div>
+    </Fragment>
+  );
+};
+
+ControlPresupuesto.propTypes = {
+  restante: PropTypes.number.isRequired,
+  presupuesto: PropTypes.number.isRequired,
+};
+
+export default ControlPresupuesto;
